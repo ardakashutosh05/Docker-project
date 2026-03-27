6fef7bab2ee8e8442e4f075fe5a801d34cb472d0
Ubuntu <ubuntu@ip-172-31-32-52.ap-southeast-1.compute.internal>
2026-03-27 11:35:51 +0000

    add-file

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask
+import redis
 import psycopg2
 import os
 
 app = Flask(__name__)
 
+redis_host = os.getenv("REDIS_HOST", "redis")
+r = redis.Redis(host=redis_host, port=6379)
+
 @app.route("/")
 def home():
-    return "🚀 Backend is running!"
+    try:
+        r.incr("hits")
+        hits = r.get("hits").decode()
+
+        return f"🚀 Visits: {hits}"
+    except Exception as e:
+        return f"Error: {e}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
